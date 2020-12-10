53d32cd22c3510c338f531e108fae31fdaf16a7a
Matheus Monteiro <matheusferretti96@gmail.com>
2020-12-10 01:03:08 +0000

    updating

--- exercises/07-Arrow-Functions/app.js
+++ exercises/07-Arrow-Functions/app.js
@@ -1,4 +1,14 @@
-
+const rapid = (str) => {
+    var newString = "";
+    var i;
+    for (i = 0; i < str.length; i++) {
+        if(str.charAt(i) != "o"){
+            newString += str.charAt(i);
+        }
+    }
+    return newString.toUpperCase();
+}
+   
    // From this line up Do not change code below
    let str = "John";
    console.log(rapid(str));
